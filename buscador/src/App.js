9b913bdfb185973b9f44035a82db7129c9196dc3
Kaua Kyoto <kauaantonioetec@gmail.com>
2023-09-01 13:36:41 +0000

    save2

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,18 +1,34 @@
-
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import './styles.css';
 
+
 function App() {
+
+  const [input, setInput] = useState('')
+
+  function aoClicar() {
+    alert('O que tem dentro do input: ' + input)
+  }
+
   return (
     <div className="container">
       <h1 className="title"> Buscador de Cep</h1>
       <div className="containerInput">
-        <input type="text" placeholder="Digite aqui..."/>
+        <input type="text" placeholder="Digite aqui..." value={input} onChange={(e) =>setInput(e.target.value)}/>
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={aoClicar}>
           <FiSearch size={25} color="#FFF"/>
         </button>
       </div>
+
+    <main className="main">
+      <h2> Cep: 19970000 </h2>
+      <span> Rua: Kyotário </span>
+      <span> Bairro: Kyotagens </span>
+      <span> Estado: Japão, Kyoto </span>
+    </main>
+
 
     </div>
   );
